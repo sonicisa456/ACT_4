a86f898255726d323b1150d187f8201e4bcae5b8
sonicisa456 <isaiasrv2019@outlook.com>
2026-02-11 13:43:13 -0600

    empezamos creando las carpetas BACKEND Y FRONTEND para asegurarnos de el orden de los archivos, luego configuramos el servidor

---
+++ BACKEND/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API ACT_4 funcionando");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log("Servidor corriendo en puerto", PORT));
